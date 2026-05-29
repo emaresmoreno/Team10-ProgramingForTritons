402774de221b668e9a5dcafa924bf461501f45b6
yuw329-collab <yuw329@ucsd.edu>
2026-05-28 18:35:57 -0700

    Add calendar function
    Add an interactive button at the bottom-right corner.
    After clicking the button, a section of calendar (using google calendar API) will be shown on the RHS of the screen. It primarily shows the events on the schedule.

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("calendarButton").addEventListener("click", function(){
+    this.style.display = "none";
+    // alert("button pressed");
+    document.getElementById("calendarPanel").classList.add("show");
+    document.getElementById("calendarPanel").style.display = "block";
+    document.getElementById("calendarPanel").style.position = "fixed";
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("calendarClose").addEventListener("click", function(){
+    document.getElementById("calendarPanel").classList.remove("show");
+    // alert("button pressed");
+    document.getElementById("calendarButton").style.display = "inline-block";
+  });
+});
+
